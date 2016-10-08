d9bc05cfaa4036e02ab1fc2dbc146eecd23da52a
thaden123 <thaden123@gmail.com>
2016-10-08 03:08:04 -0400

    Added "Only Facebook"

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/the/gi, 'Jeet');
+            var replacedText = text.replace(/Connect/gi, 'BUULLLLY!!!');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
