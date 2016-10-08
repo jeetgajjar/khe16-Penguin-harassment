581070ac318e4ca063b0cea37f22de0c0ce5a0b2
thaden123 <thaden123@gmail.com>
2016-10-08 10:29:03 -0400

    Added regex

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
 
-var elements = document.getElementsByTagName('//*[@id="content"]/div/div/div/div/div[1]/h2/text()[1]*');
+//var elements = document.getElementsByTagName('//*[@id="content"]/div/div/div/div/div[1]/h2/text()[1]*');
+
+//from Tyler
+var elements = document.getElementsByTagName('*');
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -12,23 +15,17 @@ for (var i = 0; i < elements.length; i++) {
         if (node.nodeType === 3) {
             var text = node.nodeValue;
 
-            var replacedText = text.replace(/Connect/gi, 'BUULLL89Y!!!');
-
+            //var replacedText = text.replace(/Connect/gi, 'BUULLL89Y!!!');
+			
+			//from Tyler
+			var start_sig = '<div class="_3hi clearfix"><div class="_38 direction_ltr"><span class="null"><p>';
+			var end_sig = '</p></span></div><div class="_1yr">'
+			var full_sig = '/' + start_sig + '*' + end_sig + '/gi';
+			var replacedText = text.replace(full_sig,'IT IS WORKING!');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
     }
-}
-
-function Get(url){
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url, true);
-	xhr.onreadystatechange = function() {
-	  if (xhr.readyState == 4) {
-		console.log('asdf' + xhr.responseText);
-	  }
-	}
-	xhr.send();
 }
