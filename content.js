db29ff594a75d51e9e9897ba1f24d25b571941d1
kakorrhaphio <j211668@gmail.com>
2016-10-08 10:03:19 -0400

    made some changes; broken code under content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-var elements = document.getElementsByTagName('*');
+var elements = document.getElementsByTagName('//*[@id="content"]/div/div/div/div/div[1]/h2/text()[1]*');
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/Connect/gi, 'BUULLLLY!!!');
+            var replacedText = text.replace(/Connect/gi, 'BUULLL89Y!!!');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
