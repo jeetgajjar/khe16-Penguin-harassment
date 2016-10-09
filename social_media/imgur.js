b06cfa9135442bfa368c3778343d6868df193116
kakorrhaphio <j211668@gmail.com>
2016-10-08 20:31:32 -0400

    imgur is alive

---
+++ social_media/imgur.js
@@ -0,0 +1,19 @@
+//I'm Alive!
+var elements = document.getElementsByTagName('*'); //gets all tags
+
+for (var i = 0; i < elements.length; i++) {
+   var element = elements[i];
+
+   for (var j = 0; j < element.childNodes.length; j++) {
+       var node = element.childNodes[j];
+
+       if (node.nodeType === 3) {
+           var text = node.nodeValue;
+           var replacedText = text.replace(/hi/gi, "I'm Alive");
+
+           if (replacedText !== text) {
+               element.replaceChild(document.createTextNode(replacedText), node);
+           }
+       }
+   }
+}
