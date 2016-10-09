7bef6b2e740dfed6c6939b8289ed5f9d084951b0
thaden123 <thaden123@gmail.com>
2016-10-09 04:57:30 -0400

    json from reddit

--- social_media/reddit.js
+++ social_media/reddit.js
@@ -1,22 +1,50 @@
-//I'm Alive!
-var elements = document.getElementsByTagName('*'); //gets all tags
-
-for (var i = 0; i < elements.length; i++) {
-   var element = elements[i];
-
-   for (var j = 0; j < element.childNodes.length; j++) {
-       var node = element.childNodes[j];
-
-       if (node.nodeType === 3) {
-           var text = node.nodeValue;
-           var replacedText = text.replace(/hi/gi, "I'm Alive");
-
-          // parser = new DOMParser();
-          //htmlDoc= parser.parseFromString(txt, "text/xml");
-
-           if (replacedText !== text) {
-               element.replaceChild(document.createTextNode(replacedText), node);
-           }
-       }
-   }
+function getJson(){
+	var elements = document.getElementsByTagName('div');
+	var jsonBundle = {};
+	jsonBundle['location'] = 'reddit';
+	jsonBundle['time'] = (new Date()).getTime();
+	var count = 0;
+	for (var i = 0; i < elements.length; i++) {
+	   var element = elements[i];
+	   if(element.getAttribute("class") == "md"){
+		   var textR = element.innerHTML;
+		   jsonBundle['log' + toString(count)] = {"text": textR, "local": localParse(element)}
+		   count ++;
+	   }
+	}
+	return jsonBundle;
 }
+
+function localParse(elem){
+	//elem.parentNode.replaceChild(document.createTextNode(textR), elem);
+	return "local assesment";
+}
+
+var getJSON = function(url, successHandler, errorHandler) {
+	var xhr = typeof XMLHttpRequest != 'undefined'
+		? new XMLHttpRequest()
+		: new ActiveXObject('Microsoft.XMLHTTP');
+	xhr.open('get', url, true);
+	xhr.responseType = 'json';
+	xhr.onreadystatechange = function() {
+		var status;
+		var data;
+		if (xhr.readyState == 4) { 
+			status = xhr.status;
+			if (status == 200) {
+				successHandler && successHandler(xhr.response);
+			} else {
+				errorHandler && errorHandler(status);
+			}
+		}
+	};
+	xhr.send();
+};
+
+var JSTR = JSON.stringify(getJson());
+getJSON('https://www.harrestment.com/bully/:' + JSTR, function(data) {
+	
+}, function(status) {
+	alert(JSTR);
+});
+
