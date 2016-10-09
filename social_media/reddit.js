6296f2bb53a83267377873126a7aa3b22f6de964
thaden123 <thaden123@gmail.com>
2016-10-09 08:33:24 -0400

    adding external api

--- social_media/reddit.js
+++ social_media/reddit.js
@@ -1,3 +1,4 @@
+/**
 function getJson(){
 	var elements = document.getElementsByTagName('div');
 	var jsonBundle = {};
@@ -45,6 +46,76 @@ var JSTR = JSON.stringify(getJson());
 getJSON('https://www.harrestment.com/bully/:' + JSTR, function(data) {
 	
 }, function(status) {
-	alert(JSTR);
+	alert('https://www.harrestment.com/bully/:' + JSTR);
 });
 
+function getNLP(params){
+	unirest.get("https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=" + params + "\"")
+	.header("X-Mashape-Key", "yeyrWnVD5amshxr2EnXiDEIfct4ap1XPawZjsnPKXfnAZdu4Tf")
+	.header("Accept", "application/json")
+	.end(function (result) {
+		console.log(result.status, result.headers, result.body);
+		alert(result.body);
+	});
+}*/
+
+function localParse(){
+	var elements = document.getElementsByTagName('div');
+	var count = 0;
+	for (var i = 0; i < elements.length; i++) {
+	   var element = elements[i];
+	   if(element.getAttribute("class") == "md"){
+		   var textR = "[[[" + element.innerHTML + "]]](" + getSentimentXML(parseInnerHtml(element.innerHTML)) + ")";
+		   element.parentNode.replaceChild(document.createTextNode(textR), element);
+		   count ++;
+	   }
+	}
+	return jsonBundle;
+}
+
+
+function getSentimentXML(params){
+	var jr = new XMLHttpRequest();
+	jr = open('GET', "https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=" + params + "\"",true);
+	jr.responseType = 'json';
+	jr.setRequestHeader('X-Mashape-Key','yeyrWnVD5amshxr2EnXiDEIfct4ap1XPawZjsnPKXfnAZdu4Tf');
+	jr.setRequestHeader('Accept','application/json');
+	jr.onload = function() {
+		var status;
+		var data;
+		if (xhr.readyState == 4) { 
+			status = xhr.status;
+			if (status == 200) {
+				console.log(result.status, result.headers, result.body);
+				alert(result.body.score);
+				return result.body.score;
+			} else {
+				alert('bad');
+				//errorHandler && errorHandler(status);
+			}
+		}
+	};
+	xhr.send();
+}
+
+function getSentiment(params){
+	$.getJSON( "https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=" + params + "\"", 
+	{
+		'X-Mashape-Key': 'yeyrWnVD5amshxr2EnXiDEIfct4ap1XPawZjsnPKXfnAZdu4Tf',
+		'Accept': 'application/json'
+	}).done(function(data){
+		alert(data.score);
+		return data.score;
+	});
+}
+
+function parseInnerHtml(input){
+	var txt = input.replace(/<[^]*?>/gi,'+');
+	txt = txt.replace(/\s/gi,'+');
+	txt = txt.replace(/[+]+/gi,'+');
+	alert(txt);
+	return txt;
+}
+localParse();
+//document.write('<script src="./static/jquery/jquery.js" type="text/javascript"></script>');
+//jquery(document).ready(function($){localParse();});
